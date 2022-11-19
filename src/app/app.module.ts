803b7c56944903cc76a247e0dd425cbac9511106
michelle.alanis <michelle.alanis@u-erre.mx>
2022-11-18 18:16:48 -0600

    Creación de documentos para modulo Sign Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './singup/singup.component';
+import { APP_BASE_HREF } from '@angular/common';
+
+export const appRoutes: Routes = [
+  { path: 'singup', component: SingUpComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
     LoginComponent,
-    
+    SingUpComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { useHash: false }),
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
