01d841812d6d7565968c6e45d6f55de0d5622e37
Ricardo Guzman <ricardo.guzman@gmail.com>
2022-11-22 17:31:21 -0600

    Ricardo's first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { LoginComponent } from './login/login.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 
 
@@ -10,8 +11,7 @@ const routes: Routes = [
   {path: '', component: FrontpageComponent},
   {path: 'login', component: LoginComponent},
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  
-
+  {path: 'userinfo', component: UserinfoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { LoginComponent } from './login/login.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
     LoginComponent,
+    UserinfoComponent,
     
 
   ],
