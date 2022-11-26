db3f2f9a2be073c3b407a925de3f652d09ea3279
TacxtizX <manuel.sala.alanis@u-erre.mx>
2022-11-25 23:45:44 -0600

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './singup/singup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontpageComponent,
     LoginComponent,
-    
+    SingUpComponent
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/singup/singup.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.css']
+})
+
+export class SingUpComponent implements OnInit {
+
+  formReg: FormGroup;
+
+  constructor(private UserService: UserService) {
+    this.formReg = new FormGroup({
+      name_user: new FormControl(),
+      last_name: new FormControl(),
+      email_user: new FormControl(),
+      password_user: new FormControl()
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.UserService.registro(this.formReg.value)
+    .then((response:any) => {
+      console.log(response);
+    })
+    .catch((error:any) => console.log(error));
+  }
+
+}
