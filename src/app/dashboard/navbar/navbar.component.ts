ed130f0bf589d770c1b49fff28bcd61415fd06c8
TacxtizX <manuel.sala.alanis@u-erre.mx>
2022-11-29 17:40:13 -0600

    Actualizacion

--- src/app/dashboard/navbar/navbar.component.ts
+++ src/app/dashboard/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private UserService: UserService, private router: Router) { }
 
 
   ngOnInit(): void {
+  }
+
+  logOut(){
+    this.UserService.SignOut()
+    .then(() => {
+      this.router.navigate(['./']);
+    })
+    .catch(error => console.log(error));
   }
 
 }
