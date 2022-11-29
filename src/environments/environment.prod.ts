eff80080312921e6f8ec46133f7c692419eb1978
TacxtizX <manuel.sala.alanis@u-erre.mx>
2022-11-28 19:54:42 -0600

    Actualizacion
    Login funcional [en teoria]
    Falta enrutar a dashboard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebase: {
-    projectId: 'inviapp-ur-4ea86',
-    appId: '1:681522004250:web:371790895e8700af60eca3',
-    databaseURL: 'https://inviapp-ur-4ea86-default-rtdb.firebaseio.com',
-    storageBucket: 'inviapp-ur-4ea86.appspot.com',
-    apiKey: 'AIzaSyD3ca9ThX2Y1pWsDx9QdG_w11CVQ1QlBU8',
-    authDomain: 'inviapp-ur-4ea86.firebaseapp.com',
-    messagingSenderId: '681522004250',
-    measurementId: 'G-X0QP71R80Z',
+    apiKey: "AIzaSyD3ca9ThX2Y1pWsDx9QdG_w11CVQ1QlBU8",
+    authDomain: "inviapp-ur-4ea86.firebaseapp.com",
+    databaseURL: "https://inviapp-ur-4ea86-default-rtdb.firebaseio.com",
+    projectId: "inviapp-ur-4ea86",
+    storageBucket: "inviapp-ur-4ea86.appspot.com",
+    messagingSenderId: "681522004250",
+    appId: "1:681522004250:web:371790895e8700af60eca3",
+    measurementId: "G-X0QP71R80Z"
   },
   production: true
 };
