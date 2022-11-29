0449b975996deec8657dcec151ca4e61f069adeb
TacxtizX <manuel.sala.alanis@u-erre.mx>
2022-11-29 15:54:29 -0600

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormGroup, FormControl} from '@angular/forms';
 
@@ -7,7 +7,6 @@ import { FormGroup, FormControl} from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-
 
 export class LoginComponent implements OnInit {
 
