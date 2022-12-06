764c16dfc86368c66e983b8c46f3e5d32b6d8041
Estebanpzz <juan.parra@u-erre.mx>
2022-12-06 15:58:47 -0600

    funcional

--- src/app/dashboard/products/products.component.ts
+++ src/app/dashboard/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { DataService } from 'src/app/services/data.service';
+import { Products } from '../../interfaces/products.interface';
 
 @Component({
   selector: 'app-products',
@@ -10,11 +12,44 @@ import { UserService } from 'src/app/services/user.service';
 
 export class ProductsComponent implements OnInit {
 
-  constructor(private userService: UserService, private router: Router) { }
+  productsList : Products[]=[];
+  productObj: Products = {
+    uid_user: '',
+    name_product: '',
+    id_product: '',
+    category_product: '',
+    capacity_product: 0,
+    minimumStack_product: 0
+  };
+
+  uid_user: string = '';
+  name_product: string = '';
+  id_product: string = '';
+  category_product: string = '';
+  capacity_product: number ;
+  minimumStack_product: number;
+  constructor(private userService: UserService, private router: Router, private data : DataService) { }
 
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
+
+  getAllProducts() {
+
+    this.data.getProduct().subscribe(res => {
+      this.productsList = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id_product = e.payload.doc.id_product;
+        return data;
+      })
+    }, err => {
+      alert('Error while fetching products data');
+    })
+  }
+
+
+
 
   onClick() {
     this.userService.Logout()
--- src/app/interfaces/products.interface.ts
+++ src/app/interfaces/products.interface.ts
@@ -1,7 +1,8 @@
 export interface Products {
+    id_product: string;
     uid_user: any;
     name_product: string;
-    id_product: string;
+
     category_product: string;
     capacity_product: number;
     minimumStack_product: number;
